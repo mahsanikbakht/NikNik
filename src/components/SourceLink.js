344f6b03a1869226e0a792ba17e21bb2f69e8088
mahsanikbakht <mahsanikbakht111@gmail.com>
2019-12-08 09:32:04 +0330

    my changes

---
+++ src/components/SourceLink.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SourceLink = props => {
+  return (
+    <a href='https://github.com/mahsanikbakht' target="_blank" rel="noopener noreferrer" {...props} />
+  );
+};
+
+export default SourceLink;
